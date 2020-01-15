3b6acf7c28e3c419077088533f01ae8fb85a9744
alex3660 <54926268+alex3660@users.noreply.github.com>
2020-01-15 14:06:24 -0500

    Working date hour and number

--- picoyPlaca/Form1.Designer.cs
+++ picoyPlaca/Form1.Designer.cs
@@ -41,21 +41,23 @@
             // 
             // txtNumber
             // 
-            this.txtNumber.Location = new System.Drawing.Point(58, 95);
+            this.txtNumber.Location = new System.Drawing.Point(18, 88);
             this.txtNumber.MaxLength = 4;
             this.txtNumber.Name = "txtNumber";
-            this.txtNumber.Size = new System.Drawing.Size(114, 22);
+            this.txtNumber.Size = new System.Drawing.Size(148, 22);
             this.txtNumber.TabIndex = 1;
+            this.txtNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.txtNumber.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numbreKeyPress);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(55, 66);
+            this.label1.Location = new System.Drawing.Point(15, 68);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.Size = new System.Drawing.Size(151, 17);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Numero de Placa";
+            this.label1.Text = " License Plate Number";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
@@ -64,49 +66,51 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(12, 21);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(513, 32);
+            this.label2.Size = new System.Drawing.Size(461, 32);
             this.label2.TabIndex = 5;
-            this.label2.Text = "INGRESE LOS SIGUIENTES DATOS";
+            this.label2.Text = "Submit the Following Information";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // mtxtDate
             // 
-            this.mtxtDate.Location = new System.Drawing.Point(242, 95);
+            this.mtxtDate.Location = new System.Drawing.Point(202, 88);
             this.mtxtDate.Mask = "00/00/0000";
             this.mtxtDate.Name = "mtxtDate";
-            this.mtxtDate.Size = new System.Drawing.Size(93, 22);
+            this.mtxtDate.Size = new System.Drawing.Size(103, 22);
             this.mtxtDate.TabIndex = 6;
             this.mtxtDate.ValidatingType = typeof(System.DateTime);
+            this.mtxtDate.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mtxtDate_MaskInputRejected);
             // 
             // mtxtHour
             // 
-            this.mtxtHour.Location = new System.Drawing.Point(371, 95);
+            this.mtxtHour.Location = new System.Drawing.Point(331, 88);
             this.mtxtHour.Mask = "00:00";
             this.mtxtHour.Name = "mtxtHour";
-            this.mtxtHour.Size = new System.Drawing.Size(51, 22);
+            this.mtxtHour.Size = new System.Drawing.Size(56, 22);
             this.mtxtHour.TabIndex = 7;
             this.mtxtHour.ValidatingType = typeof(System.DateTime);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(239, 66);
+            this.label3.Location = new System.Drawing.Point(199, 68);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.Size = new System.Drawing.Size(38, 17);
             this.label3.TabIndex = 8;
-            this.label3.Text = "Fecha";
+            this.label3.Text = "Date";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(368, 66);
+            this.label4.Location = new System.Drawing.Point(328, 68);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(39, 17);
             this.label4.TabIndex = 9;
-            this.label4.Text = "Hora";
+            this.label4.Text = "Hour";
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(448, 95);
+            this.btnSubmit.Location = new System.Drawing.Point(405, 78);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(77, 32);
             this.btnSubmit.TabIndex = 11;
@@ -116,9 +120,9 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(58, 169);
+            this.richTextBox1.Location = new System.Drawing.Point(33, 141);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(453, 111);
+            this.richTextBox1.Size = new System.Drawing.Size(399, 122);
             this.richTextBox1.TabIndex = 13;
             this.richTextBox1.Text = "";
             // 
@@ -126,7 +130,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 326);
+            this.ClientSize = new System.Drawing.Size(509, 300);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.label4);
@@ -145,7 +149,6 @@
         }
 
         #endregion
-        private System.Windows.Forms.TextBox txtNumber;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.MaskedTextBox mtxtDate;
@@ -154,6 +157,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnSubmit;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        public System.Windows.Forms.TextBox txtNumber;
     }
 }
 
--- picoyPlaca/Form1.cs
+++ picoyPlaca/Form1.cs
@@ -16,7 +16,7 @@ namespace picoyPlaca
         string day;
         string lastNumber;
         int numberAux;
-        
+
         public Form1()
         {
             InitializeComponent();
@@ -34,8 +34,8 @@ namespace picoyPlaca
 
         private void btnIngresar_Click(object sender, EventArgs e)
         {
-          
-            
+
+
         }
 
         private void btnSubmit_Click(object sender, EventArgs e)
@@ -59,61 +59,160 @@ namespace picoyPlaca
             Console.WriteLine(numberAux);
 
             //Hour
-            DateTime hourAux = new DateTime();            
+            DateTime hourAux = new DateTime();
             hourAux = Convert.ToDateTime(picoPlaca.Hour);
             var hourtime = hourAux.ToString("HHmm");
+            var hourtimeString = hourAux.ToString("HH:mm tt");
             int nowHours = Convert.ToInt32(hourtime);
             Console.WriteLine(nowHours);
 
 
 
             // PREDICTOR
-            if ( day == "Saturday" || day == "Sunday")
+
+            
+            // Monday
+            if (day == "Monday")
+            {
+                if (numberAux == 1 || numberAux == 2)
+                {
+                    message(nowHours);
+                }
+
+                else
+                {
+                    richTextBox1.Text = "puede Circular";
+                }
+
+
+
+            }
+
+            // Tuesday
+            else if (day == "Tuesday")
+            {
+                if (numberAux == 3 || numberAux == 4)
+                {
+                    message(nowHours);
+
+                }
+
+                else
+                {
+                    richTextBox1.Text = "puede Circular";
+                }
+
+
+
+            }
+            // Wednesday
+            else if (day == "Wednesday")
+            {
+                if (numberAux == 5 || numberAux == 6)
+                {
+                    message(nowHours);
+
+                }
+
+                else
+                {
+                    richTextBox1.Text = "puede Circular";
+                }
+
+
+
+            }
+            // Thursday
+            else if (day == "Thursday")
+            {
+                if (numberAux == 7 || numberAux == 8)
+                {
+                    message(nowHours);
+
+                }
+
+                else
+                {
+                    richTextBox1.Text = "puede Circular";
+                }
+
+
+
+            }
+            //Friday
+            else if (day == "Friday")
+            {
+                if (numberAux == 9 || numberAux == 0)
+                {
+                    message(nowHours);
+
+                }
+
+                else
+                {
+                    Console.WriteLine("puede Circular" );
+                }
+
+
+
+            }
+            //
+            else
             {
                 richTextBox1.Text = "SI PUEDE CIRCULAR TODO EL DIA EN Su Vehiculo";
 
             }
 
-            else if ( day == "Monday"  )
+
+        }
+
+        public void message(int hour) 
+        {
+            if (hour >= 0730 && hour <= 0930)
             {
-                if( numberAux == 1 || numberAux == 2)
-                {
-                    if (nowHours >= 0730 && nowHours <= 0930)
-                    {
-                        Console.WriteLine("No puede Circular desde  7:30 a 9:30");
-                        
+                richTextBox1.Text = "No puede Circular desde  7:30 a 9:30";
 
-                    }
-
-                     else if (nowHours >= 1630 && nowHours <= 1930)
-                    {
-                        Console.WriteLine("No puede Circular desde  16:30 a 19:30");
-
-
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("puede Circular");
-                    }
-
-                }
-
-                else
-                    {
-                        Console.WriteLine("puede Circular");
-                    }
-                
-                               
 
             }
 
+            else if (hour >= 1630 && hour <= 1930)
+            {
+                richTextBox1.Text = "No puede Circular desde  16:30 a 19:30";
+
+
+            }
+
+            else
+            {
+                richTextBox1.Text = "puede Circular";
+            }
         }
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
         }
 
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mtxtDate_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void numbreKeyPress(object sender, KeyPressEventArgs e)
+        {
+            Char chr = e.KeyChar;
+            if (!Char.IsDigit(chr) && chr!=8)
+            {
+                e.Handled = true;
+                MessageBox.Show("Enter a Valid Value");
+            }
+        }
     }
 }
